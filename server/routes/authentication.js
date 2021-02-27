ca86edbd055197b24182c6bae3bcfd9ca121b8da
Lucas BAUDINO <lucas.baudino@outlook.fr>
2021-02-27 16:37:06 +0100

    router.param userId

--- server/routes/authentication.js
+++ server/routes/authentication.js
@@ -2,11 +2,15 @@ const express = require("express");
 const { signup, signin, signout } = require("../controllers/authentication.js");
 const { validate } = require("../validator/validate.js");
 const { userSignupRules } = require("../validator/userSignupRules.js");
+const { userById } = require("../controllers/user.js");
 
 const router = express.Router();
 
 router.post("/signup", userSignupRules(), validate, signup);
 router.post("/signin", signin);
 router.get("/signout", signout);
+
+//any route containing :userId will execute userById
+router.param("userId", userById);
 
 module.exports = router;
