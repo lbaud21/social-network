79faa7bc14289a742c70931ce969f2113691f506
Lucas BAUDINO <lucas.baudino@outlook.fr>
2021-02-27 12:02:04 +0100

    authentication sigin route

--- server/routes/authentication.js
+++ server/routes/authentication.js
@@ -1,10 +1,11 @@
 const express = require("express");
-const { signup } = require("../controllers/authentication.js");
+const { signup, signin } = require("../controllers/authentication.js");
 const { validate } = require("../validator/validate.js");
 const { userSignupRules } = require("../validator/userSignupRules.js");
 
 const router = express.Router();
 
 router.post("/signup", userSignupRules(), validate, signup);
+router.post("/signin", signin);
 
 module.exports = router;
