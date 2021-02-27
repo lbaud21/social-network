cada43da6f847466832f90f50ae59d30e8746fdf
Lucas BAUDINO <lucas.baudino@outlook.fr>
2021-02-27 20:07:56 +0100

    get user/:userId route

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,9 +1,11 @@
 const express = require("express");
-const { userById, allUsers } = require("../controllers/user.js");
+const { userById, getAllUsers, getUser } = require("../controllers/user.js");
+const { requireSignin } = require("../controllers/authentication.js");
 
 const router = express.Router();
 
-router.get("/users", allUsers);
+router.get("/users", getAllUsers);
+router.get("/user/:userId", requireSignin, getUser);
 
 //any route containing :userId will execute userById
 router.param("userId", userById);
