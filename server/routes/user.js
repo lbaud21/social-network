0041feba68b70a27565d4ac9cb5539daf8ae7ebf
Lucas BAUDINO <lucas.baudino@outlook.fr>
2021-02-28 12:39:52 +0100

    user routes put update user

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,11 +1,17 @@
 const express = require("express");
-const { userById, getAllUsers, getUser } = require("../controllers/user.js");
+const {
+  userById,
+  getAllUsers,
+  getUser,
+  updateUser,
+} = require("../controllers/user.js");
 const { requireSignin } = require("../controllers/authentication.js");
 
 const router = express.Router();
 
 router.get("/users", getAllUsers);
 router.get("/user/:userId", requireSignin, getUser);
+router.put("/user/:userId", requireSignin, updateUser);
 
 //any route containing :userId will execute userById
 router.param("userId", userById);
