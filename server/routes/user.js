2197b9cf0e82afae5e216902aa165487bd124c4e
Lucas BAUDINO <lucas.baudino@outlook.fr>
2021-02-28 13:22:54 +0100

    . delete deleteUser route

--- server/routes/user.js
+++ server/routes/user.js
@@ -4,6 +4,7 @@ const {
   getAllUsers,
   getUser,
   updateUser,
+  deleteUser,
 } = require("../controllers/user.js");
 const { requireSignin } = require("../controllers/authentication.js");
 
@@ -12,6 +13,7 @@ const router = express.Router();
 router.get("/users", getAllUsers);
 router.get("/user/:userId", requireSignin, getUser);
 router.put("/user/:userId", requireSignin, updateUser);
+router.delete("/user/:userId", requireSignin, deleteUser);
 
 //any route containing :userId will execute userById
 router.param("userId", userById);
